b6c2cbafecc174774876386229594b83b4bfd451
timmy <kris>
2023-05-06 15:57:06 +0200

    .

--- src/main/java/dtos/InstructionDTO.java
+++ src/main/java/dtos/InstructionDTO.java
@@ -12,13 +12,20 @@ public class InstructionDTO {
     private Long id;
     private Integer stepNumber;
     private String instructionText;
+    private Recipe recipe;
 
+
+    public InstructionDTO() {
+    }
 
     public InstructionDTO(Instruction re) {
-        if(re.getId() != null)
+        if (re.getId() != null)
             this.id = re.getId();
         this.stepNumber = re.getStepNumber();
         this.instructionText = re.getInstructionText();
+        if (re.getRecipe() != null) {
+            this.recipe = re.getRecipe();
+        }
     }
 
     public InstructionDTO(Integer stepNumber, String instructionText) {
@@ -26,40 +33,32 @@ public class InstructionDTO {
         this.instructionText = instructionText;
     }
 
-    public InstructionDTO() {
-    }
 
-    public static List<InstructionDTO> getDtos(List<Instruction> instructions){
+    public static List<InstructionDTO> getDtos(List<Instruction> instructions) {
         List<InstructionDTO> rmdtos = new ArrayList();
         instructions.forEach(re -> rmdtos.add(new InstructionDTO(re)));
         return rmdtos;
     }
-
-
+    public Recipe getRecipe() { return recipe; }
+    public void setRecipe(Recipe recipe) { this.recipe = recipe; }
     public Long getId() {
         return id;
     }
-
     public void setId(Long id) {
         this.id = id;
     }
-
     public Integer getStepNumber() {
         return stepNumber;
     }
-
     public void setStepNumber(Integer stepNumber) {
         this.stepNumber = stepNumber;
     }
-
     public String getInstructionText() {
         return instructionText;
     }
-
     public void setInstructionText(String instructionText) {
         this.instructionText = instructionText;
     }
-
     @Override
     public String toString() {
         return "InstructionDTO{" +
