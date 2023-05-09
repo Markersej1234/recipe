8a170335032e4b2f90f683fb74192d97602a9099
timmy <kris>
2023-05-09 10:22:30 +0200

    .

--- src/main/java/dtos/IngredientDTO.java
+++ src/main/java/dtos/IngredientDTO.java
@@ -16,25 +16,49 @@ import java.util.List;
  * @author tha
  */
 public class IngredientDTO {
-    private int id;
+    private Long id;
     private String name;
     private Double quantity;
     private String measurementUnit;
-    private RecipeDTO recipe;
 
 
-    public IngredientDTO(String name, Double quantity, String measurementUnit, RecipeDTO recipe) {
+    public IngredientDTO(String name, Double quantity, String measurementUnit) {
         this.name = name;
         this.quantity = quantity;
         this.measurementUnit = measurementUnit;
-        this.recipe = recipe;
+
     }
 
     public IngredientDTO(Ingredient i){
+        if (i.getId() != null)
+            this.id = i.getId();
         this.name = i.getName();
         this.quantity = i.getQuantity();
         this.measurementUnit = i.getMeasurementUnit();
-        if (i.getRecipe() != null)
-        this.recipe = new RecipeDTO(i.getRecipe());
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Double getQuantity() {
+        return quantity;
+    }
+    public void setQuantity(Double quantity) {
+        this.quantity = quantity;
+    }
+    public String getMeasurementUnit() {
+        return measurementUnit;
+    }
+    public void setMeasurementUnit(String measurementUnit) {
+        this.measurementUnit = measurementUnit;
     }
 }
--- src/main/java/dtos/RecipeDTO.java
+++ src/main/java/dtos/RecipeDTO.java
@@ -5,6 +5,7 @@
  */
 package dtos;
 
+import entities.Ingredient;
 import entities.Recipe;
 import entities.RenameMe;
 
@@ -16,10 +17,11 @@ import java.util.List;
  * @author tha
  */
 public class RecipeDTO {
-    private long id;
+    private Long id;
     private String name;
     private String description;
     private String userName;
+    private List<IngredientDTO> ingredients = new ArrayList<>();
 
     public RecipeDTO(Recipe re) {
         if(re.getId() != null)
@@ -27,28 +29,38 @@ public class RecipeDTO {
         this.name = re.getName();
         this.description = re.getDescription();
         this.userName = re.getUser().getUserName();
+        for (Ingredient i : re.getIngredients()){
+            this.ingredients.add(new IngredientDTO(i));
+        }
     }
 
-    public RecipeDTO(String name, String description, String userName) {
+    public RecipeDTO(String name, String description, String userName, List<IngredientDTO> ingredients) {
         this.name = name;
         this.description = description;
         this.userName = userName;
-    }
-
-    public static List<RecipeDTO> getDtos(List<Recipe> recipes){
-        List<RecipeDTO> rmdtos = new ArrayList();
-        recipes.forEach(re -> rmdtos.add(new RecipeDTO(re)));
-        return rmdtos;
+        this.ingredients = ingredients;
     }
 
 
+    public List<IngredientDTO> getIngredients() {
+        return ingredients;
+    }
+    public void setIngredients(List<IngredientDTO> ingredients) {
+        this.ingredients = ingredients;
+    }
     public long getId() { return id; }
-    public void setId(long id) { this.id = id; }
+    public void setId(Long id) { this.id = id; }
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
     public String getDescription() { return description; }
     public void setDescription(String description) { this.description = description; }
     public String getUserName() { return userName; }
     public void setUserName(String userName) { this.userName = userName; }
+
+    public static List<RecipeDTO> getDtos(List<Recipe> recipes){
+        List<RecipeDTO> rmdtos = new ArrayList();
+        recipes.forEach(re -> rmdtos.add(new RecipeDTO(re)));
+        return rmdtos;
+    }
 
 }
